2efe07923a621906ae55d92f3d1ffde3f5ba6e8e
Andrew Lalis <andrewlalisofficial@gmail.com>
2021-05-12 09:01:55 +0200

    Added starter files.

---
+++ src/main/java/nl/andrewlalis/bot_demo/Main.java
@@ -0,0 +1,8 @@
+package nl.andrewlalis.bot_demo;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		System.out.println("Hello world!");
+		new BotDemo(args[0]);
+	}
+}
